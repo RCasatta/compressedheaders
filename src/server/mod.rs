27affc0c5ae1976f340a8cbaf619f307a348f5e7
Riccardo Casatta <riccardo@casatta.it>
2017-08-19 18:29:04 +0200

    removed unused import

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -5,7 +5,6 @@ use hyper::header::{ContentLength,ContentType};
 use hyper::server::{Http, Request, Response, Service};
 use bitcoin::header::BlockHeader;
 use hyper::StatusCode;
-use util::hex::ToHex;
 
 #[derive(Clone)]
 struct HelloWorld {
